f622e7bdfe51bfc288cc98f607a9a4d40bf5475b
Abhishek <ABHISHEKKUMAR998>
2021-06-21 13:51:23 +0530

    Using the put Command

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -3,8 +3,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,4 +33,17 @@ public class HelloWorldController {
     public String helloParam(@PathVariable String name){
         return "Hello " + name +" !!";
     }
+    
+    @PostMapping("/post")
+    public String hello(@RequestBody User user){
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " !!";
+    }
+    
+    @PutMapping("/put/{firstname}")
+    public String sayHelloPut(@PathVariable String firstname, @RequestParam(value ="lastname" )String lastname)
+    {
+    	return "Hello" +" "+ firstname + " " + lastname +
+    			" " + "From  Bridgelabz"; 
+    }
 }
+
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,26 @@
 package com.example.demo.model;
 
-public class User {
 
-}
+	public class User {
+
+		private String firstname;
+		private String lastname;
+
+
+
+		public String getFirstName()
+		{
+			return firstname;
+		}
+		public String getLastName()
+		{
+			return lastname;
+		}
+		public void setFirstName(String firstName) {
+	        this.firstname = firstName;
+	    }
+		public void setLastName(String lastName) {
+	        this.lastname = lastName;
+	    }
+	}
+
